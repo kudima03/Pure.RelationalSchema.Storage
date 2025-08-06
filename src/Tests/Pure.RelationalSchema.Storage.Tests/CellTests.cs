0d332c3960846ad980f767c957e7201d6c021330
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-06 16:37:07 +0300

    Added CellTests

---
+++ src/Tests/Pure.RelationalSchema.Storage.Tests/CellTests.cs
@@ -0,0 +1,36 @@
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.Materialized.Bool;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.String;
+using Pure.Primitives.String.Operations;
+using Pure.RelationalSchema.Storage.Abstractions;
+
+namespace Pure.RelationalSchema.Storage.Tests;
+
+public sealed record CellTests
+{
+    [Fact]
+    public void InitializeCorrectly()
+    {
+        IString cellValue = new RandomString(new UShort(10));
+
+        ICell cell = new Cell(cellValue);
+
+        Assert.True(new MaterializedBool(new EqualCondition(cellValue, cell.Value))
+            .Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() =>
+            new Cell(new RandomString(new UShort(10))).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() =>
+            new Cell(new RandomString(new UShort(10))).ToString());
+    }
+}
