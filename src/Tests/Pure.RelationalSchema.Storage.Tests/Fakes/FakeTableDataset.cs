126a1a0126dbd72c9fcbf428fb30c9cdd6c45b92
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-11 21:37:41 +0300

    Breaking changes satisfied.

--- src/Tests/Pure.RelationalSchema.Storage.Tests/Fakes/FakeTableDataset.cs
+++ src/Tests/Pure.RelationalSchema.Storage.Tests/Fakes/FakeTableDataset.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq.Expressions;
 using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.Storage.Abstractions;
 
@@ -6,15 +7,21 @@ namespace Pure.RelationalSchema.Storage.Tests.Fakes;
 
 public sealed record FakeTableDataset : IStoredTableDataSet
 {
-    private readonly IEnumerable<IRow> _rows;
+    private readonly IQueryable<IRow> _rows;
 
     public FakeTableDataset(ITable tableSchema, IEnumerable<IRow> rows)
     {
-        _rows = rows;
+        _rows = rows.AsQueryable();
         TableSchema = tableSchema;
     }
 
     public ITable TableSchema { get; }
+
+    public Type ElementType => _rows.ElementType;
+
+    public Expression Expression => _rows.Expression;
+
+    public IQueryProvider Provider => _rows.Provider;
 
     public async IAsyncEnumerator<IRow> GetAsyncEnumerator(
         CancellationToken cancellationToken = default
