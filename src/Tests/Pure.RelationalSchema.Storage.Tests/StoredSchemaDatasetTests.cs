0e5117d32b11e32fead00730b4b8b26755189a48
Dmitry Kurochkin <kudima03@gmail.com>
2025-10-15 22:53:27 +0300

    Breaking changes satisfied.

--- src/Tests/Pure.RelationalSchema.Storage.Tests/StoredSchemaDatasetTests.cs
+++ src/Tests/Pure.RelationalSchema.Storage.Tests/StoredSchemaDatasetTests.cs
@@ -1,11 +1,11 @@
 using Pure.Collections.Generic;
-using Pure.Primitives.Number;
-using Pure.Primitives.Random.String;
-using Pure.RelationalSchema.Abstractions.Column;
+using Pure.HashCodes;
+using Pure.RelationalSchema.Abstractions.Schema;
 using Pure.RelationalSchema.Abstractions.Table;
-using Pure.RelationalSchema.ColumnType;
 using Pure.RelationalSchema.HashCodes;
+using Pure.RelationalSchema.Random;
 using Pure.RelationalSchema.Storage.Abstractions;
+using Pure.RelationalSchema.Storage.HashCodes;
 using Pure.RelationalSchema.Storage.Tests.Fakes;
 
 namespace Pure.RelationalSchema.Storage.Tests;
@@ -13,55 +13,181 @@ namespace Pure.RelationalSchema.Storage.Tests;
 public sealed record StoredSchemaDatasetTests
 {
     [Fact]
-    public void InitializeCorrectly()
+    public void InitializeSchemaCorrectly()
     {
-        IReadOnlyCollection<ITable> tables =
-        [
-            new Table.Table(
-                new RandomString(new UShort(10)),
-                [
-                    new Column.Column(
-                        new RandomString(new UShort(10)),
-                        new IntColumnType()
-                    ),
-                ],
-                []
-            ),
-        ];
-
-        IReadOnlyDictionary<IColumn, ICell> dictionary = new Dictionary<
-            IColumn,
-            IColumn,
-            ICell
-        >(
-            tables.First().Columns,
-            x => x,
-            _ => new Cell(new RandomString(new UShort(10))),
-            x => new ColumnHash(x)
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
         );
 
-        IQueryable<IRow> rows = new IRow[]
+        Assert.True(
+            new SchemaHash(schema).SequenceEqual(new SchemaHash(schemaDataset.Schema))
+        );
+    }
+
+    [Fact]
+    public void InitializeCountCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.Equal(schema.Tables.Count(), schemaDataset.Count);
+    }
+
+    [Fact]
+    public void InitializeKeysCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.True(
+            new AggregatedHash(schema.Tables.Select(x => new TableHash(x))).SequenceEqual(
+                new AggregatedHash(schemaDataset.Keys.Select(x => new TableHash(x)))
+            )
+        );
+    }
+
+    [Fact]
+    public void InitializeValuesCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.True(
+            new AggregatedHash(
+                schema.Tables.Select(x => new StoredTableDataSetHash(
+                    new FakeTableDataset(x)
+                ))
+            ).SequenceEqual(
+                new AggregatedHash(
+                    schemaDataset.Values.Select(x => new StoredTableDataSetHash(x))
+                )
+            )
+        );
+    }
+
+    [Fact]
+    public void IndexingOperatorWorkCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.True(
+            new StoredTableDataSetHash(
+                new FakeTableDataset(schema.Tables.First())
+            ).SequenceEqual(
+                new StoredTableDataSetHash(schemaDataset[schema.Tables.First()])
+            )
+        );
+    }
+
+    [Fact]
+    public void ContainsWorkCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.True(schemaDataset.ContainsKey(schema.Tables.First()));
+    }
+
+    [Fact]
+    public void TryGetValueWorkCorrectly()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        Assert.True(
+            new StoredTableDataSetHash(
+                new FakeTableDataset(schema.Tables.First())
+            ).SequenceEqual(
+                new StoredTableDataSetHash(
+                    schemaDataset.TryGetValue(
+                        schema.Tables.First(),
+                        out IStoredTableDataSet? value
+                    )
+                        ? value
+                        : throw new ArgumentNullException()
+                )
+            )
+        );
+    }
+
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        ISchema schema = new RandomSchema();
+        IStoredSchemaDataSet schemaDataset = new StoredSchemaDataset(
+            schema,
+            new Dictionary<ITable, ITable, IStoredTableDataSet>(
+                schema.Tables,
+                x => x,
+                x => new FakeTableDataset(x),
+                x => new TableHash(x)
+            )
+        );
+
+        int i = 0;
+
+        foreach (object table in schemaDataset)
         {
-            new Row(dictionary),
-            new Row(dictionary),
-            new Row(dictionary),
-            new Row(dictionary),
-            new Row(dictionary),
-            new Row(dictionary),
-        }.AsQueryable();
+            i++;
+        }
 
-        IReadOnlyDictionary<ITable, IStoredTableDataSet> data = new Dictionary<
-            ITable,
-            ITable,
-            IStoredTableDataSet
-        >(
-            tables,
-            x => x,
-            x => new FakeTableDataset(tables.First(), rows),
-            x => new TableHash(x)
-        );
-
-        Assert.Equal(data, new StoredSchemaDataset(data).TablesDatasets);
+        Assert.Equal(schema.Tables.Count(), i);
     }
 
     [Fact]
@@ -69,6 +195,7 @@ public sealed record StoredSchemaDatasetTests
     {
         _ = Assert.Throws<NotSupportedException>(() =>
             new StoredSchemaDataset(
+                new RandomSchema(),
                 new Dictionary<ITable, ITable, IStoredTableDataSet>(
                     [],
                     x => x,
@@ -84,6 +211,7 @@ public sealed record StoredSchemaDatasetTests
     {
         _ = Assert.Throws<NotSupportedException>(() =>
             new StoredSchemaDataset(
+                new RandomSchema(),
                 new Dictionary<ITable, ITable, IStoredTableDataSet>(
                     [],
                     x => x,
